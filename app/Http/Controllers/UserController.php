d3a54fc08a3751102c4247dbc969edec75e38529
Uduak Essien <acushla4real@gmail.com>
2019-07-30 09:31:56 +0100

    uploaded

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -72,6 +72,7 @@ class UserController extends AppBaseController
     public function store(CreateUserRequest $request)
     {
         $input = $request->all();
+        $input['password'] = bcrypt($request->password);
         
         $date = date("Y-m-d");
 
